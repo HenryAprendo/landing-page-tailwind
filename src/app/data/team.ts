b5068bcd3e9c1c0d75f8d6e98d81ac1c3783fcd7
HenryAprendo <hjsmipuc@gmail.com>
2023-06-23 10:21:13 -0500

    Sección de testimonios en mobile

---
+++ src/app/data/team.ts
@@ -0,0 +1,47 @@
+
+export interface Team {
+  id: number;
+  name: string;
+  profile: string;
+  role: string;
+  photo: string;
+}
+
+export const team: Team[] = [
+  {
+    id: 1,
+    name: 'Satish Patel',
+    profile: 'Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.',
+    role: 'Founder & CEO, Huddle',
+    photo: './../../assets/filodark/images/profile-1.jpg'
+  },
+  {
+    id: 2,
+    name: 'Bruce McKenzie',
+    profile: 'Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.',
+    role: 'Founder & CEO, Huddle',
+    photo: './../../assets/filodark/images/profile-2.jpg'
+  },
+  {
+    id: 3,
+    name: 'Satish Patel',
+    profile: 'Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.',
+    role: 'Founder & CEO, Huddle',
+    photo: './../../assets/filodark/images/profile-3.jpg'
+  },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/app/filo-dark-project/filo-dark/filo-dark.component.ts
+++ src/app/filo-dark-project/filo-dark/filo-dark.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { dataServices, Service } from './../../data/service';
-
+import { team, Team} from './../../data/team';
 @Component({
   selector: 'app-filo-dark',
   standalone: true,
@@ -13,7 +13,7 @@ export class FiloDarkComponent {
 
   listService: Service[] = dataServices.slice();
 
-
+  team: Team[] = team.slice();
 
 }
 
